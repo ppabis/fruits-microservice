9600a39a46ff97e9d6fdea45dddd34877c507e21
ppabis <piotr@pabis.eu>
2023-04-15 18:14:27 +0200

    Token parsing and validation

---
+++ auth/auth.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"fruits_microservice/config"
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var allowedJwtMethods = []string{jwt.SigningMethodES512.Name}
+
+func Authenticate(w http.ResponseWriter, r *http.Request) *jwt.Token {
+	token := r.Header.Get("X-Auth-Token")
+	if token == "" {
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Unauthorized"))
+		return nil
+	}
+
+	tok, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return config.PublicKey, nil
+	}, jwt.WithAudience("fruits"), jwt.WithValidMethods(allowedJwtMethods))
+
+	if err != nil || !tok.Valid {
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Unauthorized"))
+		return nil
+	}
+
+	return tok
+}
--- config/config.go
+++ config/config.go
@@ -1,11 +1,45 @@
 package config
 
-import "os"
+import (
+	"crypto/ecdsa"
+	"io"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 var RedisEndpoint string = "localhost:6379"
+var PublicKey *ecdsa.PublicKey = nil
 
 func init() {
 	if os.Getenv("REDIS_ENDPOINT") != "" {
 		RedisEndpoint = os.Getenv("REDIS_ENDPOINT")
+	}
+
+	publicKeyPath := "monolith.pem"
+	if os.Getenv("PUBLIC_KEY") != "" {
+		if _, err := os.Stat(os.Getenv("PUBLIC_KEY")); err == nil {
+			publicKeyPath = os.Getenv("PUBLIC_KEY")
+		}
+	}
+
+	loadPublicKey(publicKeyPath)
+
+}
+
+func loadPublicKey(path string) {
+	publicKeyFile, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	keyBytes, err := io.ReadAll(publicKeyFile)
+	if err != nil {
+		panic(err)
+	}
+
+	PublicKey, err = jwt.ParseECPublicKeyFromPEM(keyBytes)
+	if err != nil {
+		panic(err)
 	}
 }
