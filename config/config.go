954e335823302c409c5a6255ae68c6b7524bfa77
ppabis <piotr@pabis.eu>
2023-04-15 17:51:15 +0200

    make redis endpoint configurable with env

--- config/config.go
+++ config/config.go
@@ -1,3 +1,11 @@
 package config
 
+import "os"
+
 var RedisEndpoint string = "localhost:6379"
+
+func init() {
+	if os.Getenv("REDIS_ENDPOINT") != "" {
+		RedisEndpoint = os.Getenv("REDIS_ENDPOINT")
+	}
+}
