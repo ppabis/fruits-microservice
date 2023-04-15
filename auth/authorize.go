b29eb455df6dd25db5b070b11b63ce1a6bb19567
ppabis <piotr@pabis.eu>
2023-04-15 19:31:59 +0200

    simplify protocol

--- auth/authorize.go
+++ auth/authorize.go
@@ -11,17 +11,6 @@ import (
 func Authorize(form url.Values, token *jwt.Token) error {
 	claims := token.Claims.(*jwt.MapClaims)
 
-	// Check if the subject is the same as the user in the form
-	sub, err := claims.GetSubject()
-	if err != nil {
-		return err
-	}
-
-	var user string = fmt.Sprintf("user:%s", form.Get("user"))
-	if user != sub {
-		return fmt.Errorf("user does not match token")
-	}
-
 	// Check if the user tries to set a special fruit
 	// and if the user is allowed to do so
 	super := (*claims)["super"].(bool)
