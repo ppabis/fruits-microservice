5e8acba621c0d4d87eda1ab9eb7e36ddbe863f3f
ppabis <piotr@pabis.eu>
2023-04-16 15:25:58 +0200

    rename token variable

--- auth/authenticate.go
+++ auth/authenticate.go
@@ -10,24 +10,24 @@ import (
 var allowedJwtMethods = []string{jwt.SigningMethodES512.Name}
 
 func Authenticate(w http.ResponseWriter, r *http.Request) *jwt.Token {
-	token := r.Header.Get("X-Auth-Token")
-	if token == "" {
+	token_str := r.Header.Get("X-Auth-Token")
+	if token_str == "" {
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte("Unauthorized"))
 		return nil
 	}
 
-	tok, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(token_str, func(t *jwt.Token) (interface{}, error) {
 		return config.PublicKey, nil
 	}, jwt.WithAudience("service:fruits"), jwt.WithValidMethods(allowedJwtMethods))
 
-	if err != nil || !tok.Valid {
+	if err != nil || !token.Valid {
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte("Unauthorized"))
 		return nil
 	}
 
-	return tok
+	return token
 }
